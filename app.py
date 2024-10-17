6d090c15147047ce3ad38371a9eb23b2d3700a36
Mkobie <48624400+Mkobie@users.noreply.github.com>
2024-10-17 16:20:07 +0200

    Implement new UI, implement dance move classes, and add requirements

--- app.py
+++ app.py
@@ -1,97 +1,92 @@
-import shutil
 from pathlib import Path
 
 import dash
 import dash_bootstrap_components as dbc
-from dash import html, callback, Input, Output
+from dash import html, callback, Input, Output, dcc
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
-
-assets_folder = Path.cwd() / 'assets'
-media_folder = Path.cwd() / 'media'
+from DanceMove import DanceMoveCollection
 
 # todo: add moves from
 #  https://thebluesroom.com/courses/side-by-side/
-source_names = ["Basic Turns", "Ballroom Blues", "Ballroom Blues - turns", "Close Embrace Intensive", "Close Embrace - spin"]  # refers to source of video source on Blues Room site
 
-column_files = []
-for i, source_name in enumerate(source_names):
-    video_folder = media_folder.joinpath(source_name)
-    video_files = [f.name for f in video_folder.iterdir() if f.suffix in ['.mp4', '.webm', '.ogg']]
-    column_files.append(video_files)
+grouping_titles = ["Basic turns", "Ballroom blues", "Ballroom blues - turns", "Close embrace", "Close embrace - spins"]
+assets_folder = Path.cwd() / 'assets'
 
-    for src_file in video_folder.glob('*.*'):
-        shutil.copy(src_file, assets_folder)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 
-video_files = [f.name for f in assets_folder.iterdir() if f.suffix in ['.mp4', '.webm', '.ogg']]
+dance_moves = DanceMoveCollection()
 
-
-def button_list(title, video_list):
+def button_group(title):
+    moves_for_group = [move for move in dance_moves.moves if move.grouping == title]
     return dbc.Col([
         html.H6(title, className="card-title"),
         dbc.ButtonGroup([
-            dbc.Button(video[3:].replace('.mp4', ''), id={'type': 'video-button', 'index': f"{title}_{i}"}, color="secondary",
+            dbc.Button(move.name, id={'type': 'video-button', 'index': f"{move.name}"}, color="secondary",
                        className="mb-2")
-            for i, video in enumerate(video_list)
+            for move in moves_for_group
         ], vertical=True)
-    ], className="h-100", width='auto')
+    ], className="d-grid gap-2", width=12)
 
-def generate_button_lists():
-    button_lists = []
-    for i, entry in enumerate(source_names):
-        button_lists.append(button_list(source_names[i], column_files[i]))
-    return button_lists
+def generate_button_groups():
+    button_groups = []
+    for i, entry in enumerate(grouping_titles):
+        button_groups.append(button_group(grouping_titles[i]))
+    return button_groups
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            html.H1("Title"),
+            dcc.Store(id='current-video', data=dance_moves.moves[0].name),
+            html.Video(
+                id="video-player",
+                src=f"/assets/{dance_moves.moves[0].name}.mp4",
+                controls=True,
+                autoPlay=True,
+                loop=True,
+                style={"width": "100%", "height": "auto"}
+            ),
+            html.Div("Controls"),
+        ], style={'width': '65%', 'display': 'inline-block', 'vertical-align': 'top',
+                  'padding': '20px', 'box-sizing': 'border-box'}),
+
+        html.Div([
+            dbc.Row(generate_button_groups()),
+        ], style={'width': '35%', 'display': 'inline-block', 'vertical-align': 'top',
+                  'padding': '20px', 'box-sizing': 'border-box',
+                  'height': '100vh', 'overflow-y': 'scroll'}),
+    ])
+])
 
 
-# Layout of the app
-app.layout = dbc.Container(
-    [
-        dbc.Row(
-            dbc.Col(
-                dbc.Card([
-                    dbc.CardBody([
-                        html.Video(
-                            id="video-player",
-                            src=f"/assets/{video_files[0]}",
-                            controls=True,
-                            autoPlay=True,
-                            loop=True,
-                            style={"width": "100%", "height": "auto"}
-                        )
-                    ])
-                ], className="mb-4"), width={"size": 5, "offset": 2},
-            )
-        ),
-
-        dbc.Row(generate_button_lists())
-    ], fluid=True)
-
-
-# Callback to update the video source
 @callback(
-    Output("video-player", "src"),
+[Output("video-player", "src"),
+     Output("current-video", "data")],
     Input({'type': 'video-button', 'index': dash.dependencies.ALL}, 'n_clicks'),
     prevent_initial_call=True
 )
 def update_video(n_clicks):
     ctx = dash.callback_context
-
     if not ctx.triggered:
         return dash.no_update
 
-    # Get the id of the button clicked
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-    index = eval(button_id)['index']
+    move_name = eval(button_id)['index']
 
-    # Determine the video list and file index
-    prefix, idx = index.split('_')
-    idx = int(idx)
+    return f"/assets/{move_name}.mp4", move_name
 
-    for i in range(20):
-        if prefix == source_names[i]:
-            return f"/assets/{column_files[i][idx]}"
 
-    return dash.no_update
+@app.callback(
+    Output({'type': 'video-button', 'index': dash.dependencies.ALL}, 'color'),
+    Input('current-video', 'data')
+)
+def update_button_colors(current_video):
+    # if not current_video:
+    #     return ['secondary'] * len(dance_moves.moves)  # Default color
+
+    return ['primary' if move.name == current_video else 'secondary' for move in dance_moves.moves]
+
 
 
 if __name__ == '__main__':
